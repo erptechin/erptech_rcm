8f3f6470738bb575da5db3617aad755c9bdcab79
ERP TECH <163494723+erptechin@users.noreply.github.com>
2025-03-01 05:34:28 +0530

    upd

--- erptech_rcm/public/js/delivery_note.js
+++ erptech_rcm/public/js/delivery_note.js
@@ -12,45 +12,50 @@ frappe.ui.form.on('Delivery Note', {
             };
         });
     },
-    // For My Home
-    // refresh: function (frm) {
-    //     if (frm.doc.items.length > 1) {
-    //         frappe.call({
-    //             method: "frappe.client.get",
-    //             args: {
-    //                 doctype: "Sales Order",
-    //                 name: frm.doc.items[1].against_sales_order
-    //             },
-    //             callback: function (res) {
-    //                 if (res.message) {
-    //                     frm.doc.items = [frm.doc.items[1]]
-    //                     frm.doc.items[0]['idx'] = 1
-    //                     frm.doc.items[0]['custom_pour_card'] = res.message.po_no
-    //                     frm.doc.items[0]['qty'] = frm.doc.custom_pro_qty_val0
-    //                     frm.doc.items[0]['custom_produced_qty'] = (res.message.items[0].qty - frm.doc.items[0].stock_qty)
-    //                     frm.doc.items[0]['custom_cumulative_qty'] = frm.doc.items[0]['qty'] + frm.doc.items[0]['custom_produced_qty']
-    //                     frm.refresh_field('items');
-    //                 }
-    //             }
-    //         });
-    //     }
-    // },
     refresh: function (frm) {
-        if (frm.doc.items[0] && frm.doc.items[0].against_sales_order) {
-            frappe.call({
-                method: "frappe.client.get",
-                args: {
-                    doctype: "Sales Order",
-                    name: frm.doc.items[0].against_sales_order
-                },
-                callback: function (res) {
-                    if (res.message) {
-                        frm.doc.items[0]['custom_bom_no'] = res.message.items[0] ? res.message.items[0].bom_no : ""
-                        frm.refresh_field('items');
+        // For My Home
+        let host = window.location.hostname
+        let parts = host.split('.')
+        if (parts[0] === "myhome") {
+            if (frm.doc.items.length > 1) {
+                frappe.call({
+                    method: "frappe.client.get",
+                    args: {
+                        doctype: "Sales Order",
+                        name: frm.doc.items[1].against_sales_order
+                    },
+                    callback: function (res) {
+                        if (res.message) {
+                            frm.doc.items = [frm.doc.items[1]]
+                            frm.doc.items[0]['idx'] = 1
+                            frm.doc.items[0]['custom_pour_card'] = res.message.po_no
+                            frm.doc.items[0]['qty'] = frm.doc.custom_pro_qty_val0
+                            frm.doc.items[0]['custom_produced_qty'] = (res.message.items[0].qty - frm.doc.items[0].stock_qty)
+                            frm.doc.items[0]['custom_cumulative_qty'] = frm.doc.items[0]['qty'] + frm.doc.items[0]['custom_produced_qty']
+                            frm.refresh_field('items');
+                        }
                     }
-                }
-            });
+                });
+            }
+
+        } else {
+            if (frm.doc.items[0] && frm.doc.items[0].against_sales_order) {
+                frappe.call({
+                    method: "frappe.client.get",
+                    args: {
+                        doctype: "Sales Order",
+                        name: frm.doc.items[0].against_sales_order
+                    },
+                    callback: function (res) {
+                        if (res.message) {
+                            frm.doc.items[0]['custom_bom_no'] = res.message.items[0] ? res.message.items[0].bom_no : ""
+                            frm.refresh_field('items');
+                        }
+                    }
+                });
+            }
         }
+
     },
     customer: function (frm) {
         if (frm.doc.customer) {
