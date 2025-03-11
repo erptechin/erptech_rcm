b1fcf91a1f0306618d80cfc9895179d71e7bace5
ERP TECH <163494723+erptechin@users.noreply.github.com>
2025-03-11 23:30:54 +0530

    upd

--- erptech_rcm/api/consumption.py
+++ erptech_rcm/api/consumption.py
@@ -645,5 +645,5 @@ def consumption_set_data_kyb():
                     doc.insert(ignore_permissions=True)
 
     # Commit the changes to save the records
-    frappe.db.commit()
-    return {"status": "success", "message": "Data fetched successfully"}
+    # frappe.db.commit()
+    # return {"status": "success", "message": "Data fetched successfully"}
