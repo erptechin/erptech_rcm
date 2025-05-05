ba96d13d048b821b3e06d3f1b999d9fc5f6d6d4b
ERP TECH <163494723+erptechin@users.noreply.github.com>
2025-05-05 17:56:11 +0530

    upds

--- frontend/src/app/pages/Auth/sign-in/index.jsx
+++ frontend/src/app/pages/Auth/sign-in/index.jsx
@@ -30,7 +30,7 @@ export default function SignIn() {
   } = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
-      username: "jaykusalkarenterprises@gmail.com",
+      username: "",
       password: "",
     },
   });
